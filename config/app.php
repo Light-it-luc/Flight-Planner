3575196f2bc3cc685429bbc4eb0f1b7f8158afe1
Lucas Cobham <lucascobham@hotmail.com>
2023-11-21 13:17:25 -0300

    style: remove unnecessary comment in config/app.php

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class // IDE Helper Generator for Laravel
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
     ])->toArray(),
 
     /*
