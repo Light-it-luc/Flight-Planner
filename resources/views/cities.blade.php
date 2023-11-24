98259ef53db9088aab02c5e275056632c1a87ce1
Lucas Cobham <lucascobham@hotmail.com>
2023-11-24 15:02:17 -0300

    feat: Integrate front with back. Updates in cities view and button component

--- resources/views/cities.blade.php
+++ resources/views/cities.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-6xl m-auto mt-8">
         <div class="overflow-x-auto relative">
         <table class="w-full text-sm text-center">
-            <caption style="display: none">Cities</caption>
+            <caption class="hidden">Cities</caption>
             <thead class="text-xs text-gray-800 uppercase border-b border-gray-300">
             <tr>
                 <th scope="col" class="py-3 px-6">ID</th>
@@ -10,10 +10,38 @@
                 <th scope="col" class="py-3 px-6">Country</th>
                 <th scope="col" class="py-3 px-6">Incoming Flights</th>
                 <th scope="col" class="py-3 px-6">Outgoing Flights</th>
-
             </tr>
             </thead>
             <tbody>
+                <tr class="bg-white border-b border-gray-100">
+                    <form method="POST" action="cities/create">
+                        @csrf
+                        <td class="py-4 px-6"></td>
+                        <td class="py-4 px-6">
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                required
+                                class="p-1 text-center border border-gray-300 text-gray-400
+                                placeholder-gray-300 rounded-lg">
+                        </td>
+                        <td class="py-4 px-6">
+                            <input
+                                type="text"
+                                name="country"
+                                placeholder="Country"
+                                required
+                                class="p-1 text-center border border-gray-300 text-gray-400
+                                placeholder-gray-300 rounded-lg">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td class="py-4 px-6">
+                            <x-button type="submit" class="dark:bg-gray-400 hover:bg-indigo-200">Create</x-button>
+                        </td>
+                    </form>
+                </tr>
                 @foreach($cities as $city)
                     <tr class="bg-white border-b border-gray-100">
                         <td class="py-4 px-6">{{ $city->id }}</td>
@@ -22,15 +50,24 @@
                         <td class="py-4 px-6">{{ $city->flightsTo->count() }}</td>
                         <td class="py-4 px-6">{{ $city->flightsFrom->count() }}</td>
                         <td>
-                            <x-button class="dark:bg-indigo-600 hover:bg-indigo-400">Edit</x-button>
-                            <x-button class="ml-2 dark:bg-red-600 hover:bg-red-400">Delete</x-button>
+                            <x-button :id="$city->id"
+                                      class="edit-btn dark:bg-indigo-600 hover:bg-indigo-400">
+                                Edit
+                            </x-button>
+                            <x-button :id="$city->id"
+                                      class="del-btn ml-2 dark:bg-red-600 hover:bg-red-400">
+                                Delete
+                            </x-button>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
         </div>
+    </div>
 
+    <div class="mt-12 mb-4 px-12">
+        {{ $cities->links() }}
     </div>
 </x-layout>
 
--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,9 @@
-<button {{ $attributes->merge(['class' => 'text-white font-semibold py-2 px-4 text-white rounded-xl']) }} >
+@props(['id'])
+
+<button
+    {{ $attributes->merge(['class' => 'text-white font-semibold py-2 px-4 text-white rounded-xl'])}}
+    {{ isset($id) ? 'id=' . $id : '' }}
+    {{ isset($type) ? 'type=' . $type : '' }}
+>
     {{ $slot }}
 </button>
