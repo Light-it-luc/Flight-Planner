91199efda50025f50815e48fb7c0e19bc257c359
Lucas Cobham <lucascobham@hotmail.com>
2023-11-27 19:17:43 -0300

    feat: add basic modal component

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,21 @@
+<dialog id="modal" class="w-1/4 border rounded-lg">
+    <div class="">
+        <!-- Modal Header -->
+        <div class="bg-indigo-500 text-white px-4 py-2 flex justify-between rounded-lg">
+            <h2 modal-title class="text-lg font-semibold"></h2>
+        </div>
+
+        <!-- Modal Body -->
+        <div class="p-4 py-4 mx-4 border-b">
+            <p modal-content></p>
+        </div>
+
+        <!-- Modal Footer -->
+        <div class="px-4 py-2 flex justify-end">
+            <button close-modal-btn
+                    class="bg-gray-600 hover:bg-gray-400 text-white font-semibold
+                           py-2 px-4 text-white rounded-xl w-1/4"
+                    >Close</button>
+        </div>
+    </div>
+</dialog>
