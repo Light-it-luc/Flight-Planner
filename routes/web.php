3c23db6ad8a66309ef5924e7b9f4a173307ed30a
Lucas Cobham <lucascobham@hotmail.com>
2023-11-29 16:11:11 -0300

    refactor: Modify routes/web.php to be more flexible (albeit more verbose)

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('cities', CityController::class);
+Route::group([
+    'prefix' => 'cities',
+    'controller' => CityController::class
+], function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+});
