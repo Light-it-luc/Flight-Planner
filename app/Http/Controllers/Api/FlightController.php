fdd8d8346e558a22bf7262aed233c50831256c14
Lucas Cobham <lucascobham@hotmail.com>
2023-12-19 10:17:32 -0300

    feat: implement backend functionality for flight filtering. Add boot method for automatic generation of flight N in flight model

--- app/Http/Controllers/Api/FlightController.php
+++ app/Http/Controllers/Api/FlightController.php
@@ -3,16 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCityRequest;
-use App\Models\City;
+use App\Http\Requests\GetFlightsRequest;
 use App\Models\Flight;
-use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
-    public function index(Request $request)
+    public function index(GetFlightsRequest $request)
     {
+        $all = $request->boolean('all', false);
+
+        if ($all) {
+            return Flight::select('id', 'flight_number', 'origin_city_id', 'dest_city_id', 'airline_id')
+                ->get();
+        }
+
+        $sortBy = $request->input('sort_by', 'id');
+        $asc = $request->boolean('asc', true);
+
+        $airlineId = $request->integer('airline', 0);
+        $originId = $request->integer('origin', 0);
+        $destinationId = $request->integer('destination', 0);
+
+        $departureDate = $request->string('departure', '');
+        $arrivalDate = $request->string('arrival', '');
+
         return Flight::with(['airline', 'origin', 'destination'])
+            ->order($sortBy, $asc)
+            ->filterByOrigin($originId)
+            ->filterByDestination($destinationId)
+            ->filterByAirline($airlineId)
+            ->filterByDeparture($departureDate)
+            ->filterByArrival($arrivalDate)
             ->paginate(10)
             ->withQueryString();
     }
--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Flight extends Model
 {
-    protected $guarded = ['id'];
+    protected $guarded = ['id', 'flight_number'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($flight) {
+            $flight->flight_number = static::generateFlightNumber();
+        });
+    }
 
     public function origin(): BelongsTo
     {
@@ -30,5 +41,47 @@ class Flight extends Model
         $sevenDigitNum = rand(1000000, 9999999);
 
         return $uppercaseChars->random() . $sevenDigitNum;
+    }
+
+    public function scopeOrder(Builder $query, string $sortBy, bool $ascending): void
+    {
+        $order = $ascending ? 'asc' : 'desc';
+
+        $query->orderBy($sortBy, $order);
+    }
+
+    public function scopeFilterByOrigin(Builder $query, int $originCityId): void
+    {
+        $query->when($originCityId, function($query) use ($originCityId) {
+            $query->where('origin_city_id', $originCityId);
+        });
+    }
+
+    public function scopeFilterByDestination(Builder $query, int $destCityId): void
+    {
+        $query->when($destCityId, function($query) use ($destCityId) {
+            $query->where('dest_city_id', $destCityId);
+        });
+    }
+
+    public function scopeFilterByAirline(Builder $query, int $airlineId): void
+    {
+        $query->when($airlineId, function($query) use ($airlineId) {
+            $query->where('airline_id', $airlineId);
+        });
+    }
+
+    public function scopeFilterByDeparture(Builder $query, string $departure): void
+    {
+        $query->when($departure, function ($query) use ($departure) {
+            $query->whereDate('departure_at', '=', Carbon::parse($departure));
+        });
+    }
+
+    public function scopeFilterByArrival(Builder $query, string $arrival): void
+    {
+        $query->when($arrival, function ($query) use ($arrival) {
+            $query->whereDate('arrival_at', '=', Carbon::parse($arrival));
+        });
     }
 }
